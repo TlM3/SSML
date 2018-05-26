c885c8d0b8b08513d72de04234d150aee88b8fee
Tim Godin <tim@kitebot.ca>
2018-05-26 10:15:57 -0600

    added today's prompt

---
+++ wp-05262018.js
@@ -0,0 +1,97 @@
+/* 
+
+Source https://www.reddit.com/r/WritingPrompts/comments/8m8dvf/wp_you_were_accidentally_killed_by_a_god_as/dzln8lk
+
+By u slash lucky water kid
+
+"So, how you finding it?" Luck asks, smiling awkwardly. The Gods here still don't really know how to behave around me. Officially, I am now one of them, unofficially, I should be subservient. I keep it chill, they'll get used to it.
+
+"Yeah, it's surprisingly fun!" I say placing a friendly hand on his shoulder. "I gotta ask though, how do you decide who to make lucky?"
+
+He perks up a bit "Ah well, I try to distribute it around as fair as I can, but there are some people I just really like to watch, so I give them some luck. Then there are others who I think need a bit of unluck to make improvements in their life. You see, luck isn't every..."
+
+"I see, I see," I interrupt, "But how come it seems like all the sociopath elites have all the luck? No consequences to their actions, money and fame coming at them, yet people like who I was, fought all the way."
+
+"Ah well, you see society began to develop at such a fast pace, that luck was somewhat *ahem* manufactured. I can make these 'elites' invest badly every now and then, but in a way, they themselves have become human gods" he shifts his gaze, "not like you of course."
+
+"So...I should probably be wary of the humans manufacturing my godly powers?" I smile as I refer to my subjects as humans, and then chuckle as I think of them as subjects.
+
+"I would think of it more as, be observant of what they are doing. Our power does come from their feelings for us after all."
+
+"Can you make gods lucky?"
+
+"Alas no. We are immune to godly influences."
+
+"Seems a bit unfair if Fire can just burn your face off."
+
+"Well, being immortal does tend to make that an irrelevant worry."
+
+"Cool." I nod. Not a bad gig. I better thank Gravity for that sneeze of hers. "So you just wanted to talk about how things were going?"
+
+"Err...Not quite." Luck takes a deep breath. "The other gods and I are concerned that you are not using your powers in a fair way."
+
+"I am the most fair!" I scoff.
+
+"You cannot inflict your powers too strongly on people."
+
+"Some deserve it. And what of it anyway? I can do what I was made to do, and it seems you cannot stop me."
+
+"No...the entire human race fears you, and that makes us fear you, for no other has had power such as yours."
+
+I smile and place both my hands on his shoulders. I feel my power flow through me, and I see the change in Luck's eyes. I have affected him. I am above all.
+
+"I am sorry." Luck says, eyes watering. "I should not have been so blunt...I owe you so much."
+
+I remove my hands from him and laugh.
+
+"I always collect." I whisper, fully embracing my new name. Debt.
+
+*/
+
+<p>"<say-as interpret-as="interjection">So</say-as>, how you finding it?" <phoneme alphabet="ipa" ph="lʌˌk">Luck</phoneme> asks, smiling awkwardly. The Gods here still don't really know how to behave around me. Officially, I am now one of them. Unofficially, I should be subservient. I keep it chill. Theyll get used to it.</p>
+
+
+<p>"<say-as interpret-as="interjection">Yeah</say-as>, it's surprisingly fun!" I say, placing a friendly hand on his shoulder. "I gotta ask though, how do you decide who to make lucky?"</p>
+
+<p>He perks up a bit. "Ah, well, I try to distribute it around as fair as I can, but there are some people I just really like to watch, so I give them some luck. Then there are others who I think need a bit of unluck to make improvements in their life. You see, luck isn't <prosody rate="x-slow">every...</prosody>"</p>
+
+<p>"I see, I see," I interrupt, "But how come it seems like all the sociopath elites have all the luck? No consequences to their actions, money and fame coming at them, yet people like who I was, fought all the way."</p>
+
+<p>"Ah, well, you see? society began to develop at such a fast pace, that luck was somewhat, *ahem*, manufactured. I can make these 'elites' invest badly every now and then, but in a way, they themselves have become human gods." He shifts his gaze, "not like you, of course."</p>
+
+<p>"So...I should probably be wary of the humans manufacturing my godly powers?" I smile, as I refer to my subjects as humans, and then chuckle as I think of them as subjects.</p>
+
+<p>"I would think of it more as 'be observant of what they are doing.' Our power does come from their feelings for us, after all."</p>
+
+<p>"Can you make gods lucky?"</p>
+
+<p>"Alas, no. We are immune to godly influences."</p>
+
+<p>"Seems a bit unfair, if '<prosody rate="x-slow">Fire</prosody>' can just burn your face off."</p>
+
+<p>"Well, being immortal <prosody rate="x-slow">does</prosody> tend to make that an irrelevant worry."</p>
+
+<p>"Cool." I nod. Not a bad gig. I better thank Gravity for that "sneeze" of hers.</p> <p>"So, you just wanted to talk about how things were going?"</p>
+
+<p>"Err...Not quite." <phoneme alphabet="ipa" ph="lʌˌk">Luck</phoneme> takes a deep breath. "The other gods and I are concerned that you are not using your powers in a fair way."</p>
+
+<p>"I am the <emphasis level="strong">most</emphasis> fair!" I scoff.</p>
+
+<p>"You cannot inflict your powers too strongly on people."</p>
+
+<p>"Some deserve it. And what of it anyway? I can do what I was made to do, and it seems you cannot stop me."</p>
+
+<p>"No...the entire human race fears you, and that makes <say-as interpret-as="interjection"><emphasis level="strong">"us"</emphasis></say-as> fear you, for no other has had power such as yours."</p>
+
+<p>I smile and place both my hands on his shoulders. I feel my power flow through me, and I see the change in <phoneme alphabet="ipa" ph="lʌˌks">Luck's</phoneme> eyes. I have affected him. I am <emphasis level="strong">above all</emphasis>.</p>
+
+<p>"I am sorry," <phoneme alphabet="ipa" ph="lʌˌk">Luck</phoneme> says, eyes watering. "I should not have been so blunt...I owe you so much."</p>
+
+<p>I remove my hands from him and laugh.</p>
+
+<p>"I always collect." I whisper, fully embracing my new name.</p><p>Debt.</p>
+
+
+//One line it. Add escape characters.
+
+<p>"<say-as interpret-as="interjection">So</say-as>, how you finding it?" <phoneme alphabet="ipa" ph="lʌˌk">Luck</phoneme> asks, smiling awkwardly. The Gods here still don\'t really know how to behave around me. Officially, I am now one of them. Unofficially, I should be subservient. I keep it chill. Theyll get used to it.</p><p>"<say-as interpret-as="interjection">Yeah</say-as>, it\'s surprisingly fun!" I say, placing a friendly hand on his shoulder. "I gotta ask though, how do you decide who to make lucky?"</p><p>He perks up a bit. "Ah, well, I try to distribute it around as fair as I can, but there are some people I just really like to watch, so I give them some luck. Then there are others who I think need a bit of unluck to make improvements in their life. You see, luck isn\'t <prosody rate="x-slow">every...</prosody>"</p><p>"I see, I see," I interrupt, "But how come it seems like all the sociopath elites have all the luck? No consequences to their actions, money and fame coming at them, yet people like who I was, fought all the way."</p><p>"Ah, well, you see? society began to develop at such a fast pace, that luck was somewhat, *ahem*, manufactured. I can make these \'elites\' invest badly every now and then, but in a way, they themselves have become human gods." He shifts his gaze, "not like you, of course."</p><p>"So...I should probably be wary of the humans manufacturing my godly powers?" I smile, as I refer to my subjects as humans, and then chuckle as I think of them as subjects.</p><p>"I would think of it more as \'be observant of what they are doing.\' Our power does come from their feelings for us, after all."</p><p>"Can you make gods lucky?"</p><p>"Alas, no. We are immune to godly influences."</p><p>"Seems a bit unfair, if \'<prosody rate="x-slow">Fire</prosody>\' can just burn your face off."</p><p>"Well, being immortal <prosody rate="x-slow">does</prosody> tend to make that an irrelevant worry."</p><p>"Cool." I nod. Not a bad gig. I better thank Gravity for that "sneeze" of hers.</p> <p>"So, you just wanted to talk about how things were going?"</p><p>"Err...Not quite." <phoneme alphabet="ipa" ph="lʌˌk">Luck</phoneme> takes a deep breath. "The other gods and I are concerned that you are not using your powers in a fair way."</p><p>"I am the <emphasis level="strong">most</emphasis> fair!" I scoff.</p><p>"You cannot inflict your powers too strongly on people."</p><p>"Some deserve it. And what of it anyway? I can do what I was made to do, and it seems you cannot stop me."</p><p>"No...the entire human race fears you, and that makes <say-as interpret-as="interjection"><emphasis level="strong">"us"</emphasis></say-as> fear you, for no other has had power such as yours."</p><p>I smile and place both my hands on his shoulders. I feel my power flow through me, and I see the change in <phoneme alphabet="ipa" ph="lʌˌks">Luck\'s</phoneme> eyes. I have affected him. I am <emphasis level="strong">above all</emphasis>.</p><p>"I am sorry," <phoneme alphabet="ipa" ph="lʌˌk">Luck</phoneme> says, eyes watering. "I should not have been so blunt...I owe you so much."</p><p>I remove my hands from him and laugh.</p><p>"I always collect." I whisper, fully embracing my new name.</p><p>Debt.</p>
